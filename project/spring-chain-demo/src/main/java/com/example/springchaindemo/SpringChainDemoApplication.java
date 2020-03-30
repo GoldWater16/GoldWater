21d54628565ce612e94bafbccc708edfc44124fe
HuGoldWater <huzewu@photonpay.com>
2020-03-30 16:38:31 +0800

    添加基于spring实现责任链代码

---
+++ project/spring-chain-demo/src/main/java/com/example/springchaindemo/SpringChainDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springchaindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChainDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChainDemoApplication.class, args);
+	}
+
+}
---
+++ project/spring-chain-demo/src/test/java/com/example/springchaindemo/SpringChainDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springchaindemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChainDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
