8073518bbbcfbe1a2c4176f3759a20246484400b
HuGoldWater <huzewu@photonpay.com>
2020-04-13 00:10:44 +0800

    两数之和代码

---
+++ LeetCode/src/main/java/com/hjs/twosum/twoSumSolution.java
@@ -0,0 +1,34 @@
+package com.hjs.twosum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @projectName: LeetCode
+ * @className: twoSumSolution
+ * @description: 给定 nums = [2, 7, 11, 15], target = 9
+ * @author: HuGoldWater
+ * @create: 2020-04-12 23:44
+ **/
+public class twoSumSolution {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 9- 2 =7
+            // 9- 7 =2
+            int s = target - nums[i];
+            if(map.containsKey(s)){
+                return new int[]{map.get(s),i};
+            }else {
+                map.put(nums[i],i);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{2,7,11,15},26)));
+    }
+}
